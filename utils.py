e5f4df06616625a0a9ff564fd345f8cd085f3c9d
hkair <hb2kang@gmail.com>
2021-01-02 03:40:27 -0800

    Add function to save all video to pytorch tensor

--- utils.py
+++ utils.py
@@ -136,17 +136,125 @@ class VideoFilePathToTensor(object):
         cap.release()
         return frames
 
-if __name__ == "__main__":
-    basketball_dataset = BasketballDataset(annotation_dict="dataset/annotation_dict.json",
-                                label_dict="dataset/labels_dict.json",
-                                transform=transforms.Compose([VideoFilePathToTensor(max_len=16, fps=10, padding_mode='last')]),
-                                poseData=True)
+class BasketballDatasetTensor(Dataset):
+    """SpaceJam: a Dataset for Basketball Action Recognition."""
 
-    print(len(basketball_dataset))
-    with open("dataset/annotation_dict.json") as f:
+    def __init__(self, annotation_dict, video_dir="dataset/examples/", data_dir="tensor-dataset/", transform=None, poseData=False):
+        with open(annotation_dict) as f:
+            self.video_list = list(json.load(f).items())
+
+        self.video_dir = video_dir
+        self.data_dir = data_dir
+        self.poseData = poseData
+        self.transform = transform
+
+    def __len__(self):
+        # return length of none-flipped videos in directory
+        return len(self.video_list)
+
+    def __getitem__(self, idx):
+        video_id = self.video_list[idx][0]
+
+        encoding = np.squeeze(np.eye(10)[np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).reshape(-1)])
+        if self.poseData:
+            joints = np.load(self.video_dir + video_id + ".npy", allow_pickle=True)
+            sample = {'joints': joints, 'action': encoding[self.video_list[idx][1] - 1]}
+        else:
+            video = torch.load(self.data_dir + video_id + ".pt")
+            sample = {'video': video, 'action': encoding[self.video_list[idx][1] - 1]}
+
+        return sample
+
+def VideoToTensor(video_id, data_dir="dataset/examples/", output_dir="tensor-dataset/", max_len=None, fps=None, padding_mode=None):
+    # open video file
+    path = data_dir + video_id + ".mp4"
+    cap = cv2.VideoCapture(path)
+    assert (cap.isOpened())
+
+    channels = 3
+
+    # calculate sample_factor to reset fps
+    sample_factor = 1
+    if fps:
+        old_fps = cap.get(cv2.CAP_PROP_FPS)  # fps of video
+        sample_factor = int(old_fps / fps)
+        assert (sample_factor >= 1)
+
+    # init empty output frames (C x L x H x W)
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    time_len = None
+    if max_len:
+        # time length has upper bound
+        if padding_mode:
+            # padding all video to the same time length
+            time_len = max_len
+        else:
+            # video have variable time length
+            time_len = min(int(num_frames / sample_factor), max_len)
+    else:
+        # time length is unlimited
+        time_len = int(num_frames / sample_factor)
+
+    frames = torch.FloatTensor(channels, time_len, height, width)
+
+    for index in range(time_len):
+        frame_index = sample_factor * index
+
+        # read frame
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+        ret, frame = cap.read()
+        if ret:
+            # successfully read frame
+            # BGR to RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = torch.from_numpy(frame)
+            # (H x W x C) to (C x H x W)
+            frame = frame.permute(2, 0, 1)
+            frames[:, index, :, :] = frame.float()
+        else:
+            # reach the end of the video
+            if padding_mode == 'zero':
+                # fill the rest frames with 0.0
+                frames[:, index:, :, :] = 0
+            elif padding_mode == 'last':
+                # fill the rest frames with the last frame
+                assert (index > 0)
+                frames[:, index:, :, :] = frames[:, index - 1, :, :].view(channels, 1, height, width)
+            break
+
+    frames /= 255
+    cap.release()
+    torch.save(frames, output_dir + video_id + ".pt")
+
+def convertAllVideo(path="dataset/annotation_dict.json"):
+    # Let's convert all video to .pt files
+    with open(path) as f:
         video_list = list(json.load(f).items())
-    print(len(video_list))
-    print(basketball_dataset[1]['joints'])
 
+    i = 1
+    for video_id in video_list:
+        print(video_id[0])
+        print("Video: ", i)
+        print(i/37085)
+        VideoToTensor(video_id[0], max_len=16, fps=10, padding_mode='last')
+        i += 1
 
+if __name__ == "__main__":
+    # basketball_dataset = BasketballDataset(annotation_dict="dataset/annotation_dict.json",
+    #                             label_dict="dataset/labels_dict.json",
+    #                             transform=transforms.Compose([VideoFilePathToTensor(max_len=16, fps=10, padding_mode='last')]),
+    #                             poseData=True)
 
+    basketball_dataset = BasketballDatasetTensor(annotation_dict="dataset/annotation_dict.json",
+                                                poseData=False)
+
+    # print(len(basketball_dataset))
+    # with open("dataset/annotation_dict.json") as f:
+    #     video_list = list(json.load(f).items())
+    # print(len(video_list))
+    # print(basketball_dataset[1]['video'].shape)
+
+    convertAllVideo()
